6a69af14d961920e49d4c26a918f2afdd04a88b7
oscar <cs.oscarchavarriaga@gmail.com>
2022-10-06 16:41:14 +0000

    developing your first ml workflow, designing your first workflow finished

--- lesson3/HelloBlazePreprocessLambda.py
+++ lesson3/HelloBlazePreprocessLambda.py
@@ -5,8 +5,8 @@ import os
 import boto3
 from botocore.exceptions import ClientError
 
-BUCKET_NAME = 'FILL_THIS_IN'
-PREFIX = 'FILL_THIS_IN'
+BUCKET_NAME = 'sagemaker-essentials-bucket'
+PREFIX = 'l3hbpl'
 
 # Function below unzips the archive to the local directory. 
 
